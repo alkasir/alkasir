71e0ea727f81bd68c132e7b5ed32dda1b1c0de05
Thomas Frössman <thomasf@jossystem.se>
2016-08-18 14:07:56 +0200

    Fix deps install path for go-bindata-assetfs

--- make.go
+++ make.go
@@ -823,7 +823,7 @@ func ReleaseChromeExtTask() {
 // install tools required by build process
 func toolsTask() {
 	runCmd("go", "install",
-		"github.com/alkasir/alkasir/vendor/github.com/elazarl/go-bindata-assetfs",
+		"github.com/alkasir/alkasir/vendor/github.com/elazarl/go-bindata-assetfs/go-bindata-assetfs",
 		"github.com/alkasir/alkasir/vendor/github.com/jteeuwen/go-bindata/go-bindata",
 	)
 }
